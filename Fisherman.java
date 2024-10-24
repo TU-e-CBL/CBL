6dca85f83b52574bd6f2466fd87b8380869f2f4f
Ivanov <stanislav.m.ivanov.2019@elsys-bg.org>
2024-10-24 22:04:00 +0200

    Fisherman makes sound when talking

--- Fisherman.java
+++ Fisherman.java
@@ -3,9 +3,15 @@ package CBL;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JOptionPane;
 
 public class Fisherman extends GameObject {
@@ -17,7 +23,7 @@ public class Fisherman extends GameObject {
     public Fisherman(int x, int y) {
         super(x, y, 100, 100);
         dialogue = loadDialogue("Hello_there.txt");
-        textPanel = new CustomTextPanel("");
+        panel = new CustomTextPanel();
     }
 
     private String[] loadDialogue(String filePath) {
@@ -41,12 +47,25 @@ public class Fisherman extends GameObject {
     public void interact() {
         spoken = true;
         for (String line : dialogue) { 
+            playSound("bruh.wav");  // Play sound before showing dialogue
             JOptionPane.showMessageDialog(null, line, null, JOptionPane.PLAIN_MESSAGE);
         }
         Play.upPressed = false;
         Play.downPressed = false;
         Play.leftPressed = false;
         Play.rightPressed = false;
+    }
+
+    private void playSound(String soundFilePath) {
+        try {
+            File soundFile = new File(soundFilePath); // Specify the path to the sound file
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start(); // Play the sound
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            System.err.println("Error playing sound: " + e.getMessage());
+        }
     }
 
     @Override
